26e7e1ec7e263555fe6e65c721609919f7066786
kseysh <69035864+kseysh@users.noreply.github.com>
2022-12-03 21:28:57 +0900

    Add files via upload

---
+++ 10250.py
@@ -0,0 +1,10 @@
+num=int(input())
+for i in range(num):
+	h,w,n=map(int,input().split())
+	if n%h!=0:
+		floor=n//h+1
+		count=n%h
+	else:
+		floor=n//h
+		count=h
+	print(str(count)+str(floor).zfill(2))
