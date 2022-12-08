1c6fbde814ca6b30b55fe70e619d5733746571e3
kseysh <69035864+kseysh@users.noreply.github.com>
2022-12-08 16:47:50 +0900

    Add files via upload

---
+++ 2941.py
@@ -0,0 +1,12 @@
+alp=input()
+
+alp=alp.replace('dz=','a')
+alp=alp.replace('z=','a')
+alp=alp.replace('c=','a')
+alp=alp.replace('s=','a')
+alp=alp.replace('nj','a')
+alp=alp.replace('lj','a')
+alp=alp.replace('d-','a')
+alp=alp.replace('c-','a')
+
+print(len(alp))
